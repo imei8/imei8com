86a7e6b76e9479c2e724afd357557a27aaf40814
imei8 <ynland@live.cn>
2014-02-20 08:53:44 +0800

    imei8

---
+++ ExcelToSqlserver/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ExcelToSqlserver.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>XML操作处理</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        中文表名：<asp:TextBox ID="txtCname" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        数据表名：<asp:TextBox ID="txtEName" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="添加" />
+&nbsp;
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="修改" />
+&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="删除" />
+&nbsp;&nbsp;
+        <asp:Button ID="btnCheck" runat="server" OnClick="btnCheck_Click" Text="检查" />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ExcelToSqlserver/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using CommonLib;
+
+namespace ExcelToSqlserver
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 添加节点
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XMLBase xmlBase = new XMLBase();
+            xmlBase.LoadDome();
+            xmlBase.InsertNode(txtCname.Text,txtEName.Text);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            XMLBase xmlBase = new XMLBase();
+            xmlBase.LoadDome();
+            bool res = xmlBase.UpdateNode(txtCname.Text, txtEName.Text);
+            Label1.Text = res.ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCheck_Click(object sender, EventArgs e)
+        {
+            XMLBase xmlBase = new XMLBase();
+            xmlBase.LoadDome();
+           bool res= xmlBase.CheckNode(txtCname.Text, txtEName.Text);
+           Label1.Text = res.ToString();
+        }
+    }
+}
